ffe3091da3e38d5d18f380fe8731b416ee43935b
Bryan <Bryan@DESKTOP-9CJ4E44>
2022-08-26 00:11:23 -0400

    Add project files.

---
+++ Hello World Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Hello_World_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "Hello,World!";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter Username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter User's Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username Is: " + userName + " And The Age Is " + age );
+        }
+        
+    }
+}
